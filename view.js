da47113b762accb6567594b7ffd1f82093eefc1e
DChaurio <70455014+DChaurio@users.noreply.github.com>
2021-04-22 11:32:12 -0400

    Create view.js

---
+++ view.js
@@ -0,0 +1,17 @@
+export default class View {
+	constructor(){
+		this.model=null;
+		this.table= document.getElementById("table");
+		const btn= document.getElementById("add");
+		btn.onclick= () => this.addTodo("Titulo", "Descripcion");
+		
+	}
+
+	setModel(model){
+		this.model=model;
+	}
+
+	addTodo(title, description){
+		this.model.addTodo(title,description);
+	}
+}
