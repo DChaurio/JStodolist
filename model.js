ae23af421901ef92a33194adbbb5ea88a0350cd0
DChaurio <70455014+DChaurio@users.noreply.github.com>
2021-04-22 11:31:49 -0400

    Create model.js

---
+++ model.js
@@ -0,0 +1,20 @@
+export default class Model {
+	constructor(){
+		this.view= null;
+		this.todos=[];
+	}
+
+	setView(view){
+		this.view= view;
+	}
+
+	getTodo(){
+		return this.todos;
+	}
+
+	addTodo(title, description){
+		const todo={
+			
+		}
+	}
+}
