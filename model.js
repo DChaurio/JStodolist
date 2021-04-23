8da54dd305f530e7cb1be6c32976bd2743c46eb5
DChaurio <70455014+DChaurio@users.noreply.github.com>
2021-04-23 16:56:24 -0400

    Update model.js

--- model.js
+++ model.js
@@ -2,6 +2,7 @@ export default class Model {
 	constructor(){
 		this.view= null;
 		this.todos=[];
+		this.currentId=1;
 	}
 
 	setView(view){
@@ -14,7 +15,16 @@ export default class Model {
 
 	addTodo(title, description){
 		const todo={
-			
+			id=this.currentId++,
+			title,
+			description,
+			completed:false,
 		}
-	}
+
+		this.todos.push(todo);
+		console.log(this.todos);
+
+		return {...todo};
+		}
+	
 }
