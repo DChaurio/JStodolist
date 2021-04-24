eb3a54f5d80ab4b071175459b56fa03d12d1d30e
DChaurio <70455014+DChaurio@users.noreply.github.com>
2021-04-24 14:30:00 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,5 +5,7 @@ document.addEventListener('DOMContentLoaded', function(){
 	const model= new Model();
 	const view= new View();
 	model.setView(view);
-	view.setModel(model);
+    view.setModel(model);
+    
+    view.render();
 });
