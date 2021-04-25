ccd9eb155c749029f81be4b13be2afb127637243
DChaurio <70455014+DChaurio@users.noreply.github.com>
2021-04-25 13:56:35 -0400

    Update add-todo.js

--- js/components/add-todo.js
+++ js/components/add-todo.js
@@ -1,15 +1,20 @@
+import Alert from "./alert.js"
+
 export default class AddTodo {
     constructor(){
 		this.btn= document.getElementById("add");
         this.title= document.getElementById("title");
-	    this.description= document.getElementById("description");
+        this.description= document.getElementById("description");
+        
+        this.alert= new Alert("alert");
     }
 
     onClick(callback){
         this.btn.onclick= () =>{
             if (title.value === "" || description.value === ""){
-                console.error("Incorrecto");
+                this.alert.show("The title and description are requierd");
 		}   else {
+            this.alert.hide();
             callback(this.title.value, this.description.value);
         }
         }
